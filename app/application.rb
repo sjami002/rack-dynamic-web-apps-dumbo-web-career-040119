97add6270d8e23d61d68be9eed0a5993f499213b
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 19:41:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
     resp.finish
   end
 
 end
+
+
